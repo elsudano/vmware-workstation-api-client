d94924672578048eaeaf8e121c130352a03d8a4b
Carlos Ariel de la Torre <carlos.ariel.de.la.torre@gmail.com>
2024-03-05 23:10:10 +0100

    test: We have added the first test for the client

--- wsapiclient/wsapiclient_test.go
+++ wsapiclient/wsapiclient_test.go
@@ -1,14 +1,20 @@
 package wsapiclient
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewClient(t *testing.T) {
-	_, err := NewClient("", "", "", false, false)
+	url, user, pass, _, insecure, debug, err := InitialData("../config.ini")
 	if err != nil {
 		t.Errorf("%v\n", err)
 	}
-}
-
-func TestRequestCurl(t *testing.T) {
-
+	apiClient, err := NewClient(url, user, pass, insecure, debug)
+	if err != nil {
+		t.Errorf("%v\n", err)
+	}
+	VM, err := apiClient.GetVM("545OMDAL1R520604HKNKA6TTK6TBNOHK")
+	if VM.Denomination != "parentvm" || err != nil {
+		t.Errorf("[ERROR][WSAPICLI] Fi: wsapiclient_test.go Fu: TestNewClient M: You need make sure that the ParentVM it's called 'parentvm' %#v", err)
+	}
 }
