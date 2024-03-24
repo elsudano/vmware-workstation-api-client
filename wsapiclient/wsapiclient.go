87844ea32f77228dc3f20a8eca853111f3c1a866
Carlos Ariel de la Torre <carlos.ariel.de.la.torre@gmail.com>
2024-03-24 19:45:12 +0100

    feat: We have created the new version (1.0.16) of the API Client

--- wsapiclient/wsapiclient.go
+++ wsapiclient/wsapiclient.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	libraryVersion  = "1.0.15"
+	libraryVersion  = "1.0.16"
 	defaultUser     = "Admin"
 	defaultPassword = "Adm1n#00"
 	defaultBaseURL  = "https://localhost:8697/api"
