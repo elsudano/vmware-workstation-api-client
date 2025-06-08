5368d4c6d37f3cb235bfc0474d31a3aec33de548
Carlos Ariel de la Torre <carlos.ariel.de.la.torre@gmail.com>
2025-06-08 19:39:20 +0200

    chore: We have created a new object to manage the Network Information

--- wsapinet/types.go
+++ wsapinet/types.go
@@ -1,13 +1,16 @@
 package wsapinet
 
-import "github.com/elsudano/vmware-workstation-api-client/httpclient"
+import (
+	"github.com/elsudano/vmware-workstation-api-client/httpclient"
+	"github.com/elsudano/vmware-workstation-api-client/wsapivm"
+)
 
 // Interface with all the methods that we can use to talk with the API of VmWare Workstation Pro
 type NETService interface {
-	CreateNIC(t string, vnet string) (*InfoNICS, error)
-	LoadNICS(vnet string) (*InfoNICS, error)
-	UpdateNIC(nic *InfoNICS, t string, vnet string) error
-	DeleteNIC(nic *InfoNICS) error
+	CreateNIC(vm *wsapivm.MyVm, t string, vnet string) (*InfoNICS, error)
+	LoadNICS(vm *wsapivm.MyVm) (*InfoNICS, error)
+	UpdateNIC(vm *wsapivm.MyVm, inx int32, t string, vnet string) (*InfoNICS, error)
+	DeleteNIC(vm *wsapivm.MyVm, inx int32) error
 }
 
 // That's the Manager to make the calls
@@ -19,7 +22,7 @@ type NETManager struct {
 type InfoNICS struct {
 	Num  int `json:"num"`
 	NICS []struct {
-		Index int    `json:"index"`
+		Index int32  `json:"index"`
 		Type  string `json:"type"`
 		Vmnet string `json:"vmnet"`
 		Mac   string `json:"macAddress"`
@@ -27,7 +30,59 @@ type InfoNICS struct {
 }
 
 // This is the information that we need to use in order to create a new NIC
-type nicPayload struct {
+type NicPayload struct {
 	Type  string `json:"type"`
 	Vmnet string `json:"vmnet"`
+}
+
+// This's the complete information of Network on a VM
+type InfoNetwork struct {
+	Nics []struct {
+		Mac string   `json:"mac"`
+		Ip  []string `json:"ip"`
+		Dns struct {
+			Hostname   string   `json:"hostname"`
+			Domainname string   `json:"domainname"`
+			Server     []string `json:"server"`
+			Search     []string `json:"search"`
+		}
+		Wins struct {
+			Primary   string `json:"primary"`
+			Secondary string `json:"secondary"`
+		}
+		Dhcp4 struct {
+			Enabled  bool   `json:"enabled"`
+			Settings string `json:"settings"`
+		}
+		Dhcp6 struct {
+			Enabled  bool   `json:"enabled"`
+			Settings string `json:"settings"`
+		}
+	}
+	Routes []struct {
+		Dest      string `json:"dest"`
+		Prefix    int32  `json:"prefix"`
+		Nexthop   string `json:"nexthop"`
+		Interface int32  `json:"interface"`
+		Type      int32  `json:"type"`
+		Metric    int32  `json:"metric"`
+	}
+	Dns struct {
+		Hostname   string   `json:"hostname"`
+		Domainname string   `json:"domainname"`
+		Server     []string `json:"server"`
+		Search     []string `json:"search"`
+	}
+	Wins struct {
+		Primary   string `json:"primary"`
+		Secondary string `json:"secondary"`
+	}
+	Dhcpv4 struct {
+		Enabled  bool   `json:"enabled"`
+		Settings string `json:"settings"`
+	}
+	Dhcpv6 struct {
+		Enabled  bool   `json:"enabled"`
+		Settings string `json:"settings"`
+	}
 }
