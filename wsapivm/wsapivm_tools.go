4f2c3110d1e14374255614be02f535300da8241b
Carlos Ariel de la Torre <carlos.ariel.de.la.torre@gmail.com>
2025-06-08 19:40:16 +0200

    feat: We have create a coupe of new methods to split tasks when we create a new VM

--- wsapivm/wsapivm_tools.go
+++ wsapivm/wsapivm_tools.go
@@ -5,13 +5,54 @@ import (
 	"encoding/json"
 
 	"github.com/elsudano/vmware-workstation-api-client/httpclient"
-	"github.com/elsudano/vmware-workstation-api-client/wsapinet"
 	"github.com/rs/zerolog/log"
 )
 
+// CloneVM Auxiliary function that allow us to clone a VM in a new one
+// just with the the same settings that the ParentVM
+// Imputs:
+// vmc: (*httpclient.HTTPClient) pointer at the client of the API server.
+// pid (string) Chain with the ID of the Parent VM
+// n: (string) Chain with the name of the new VM
+// Outputs:
+// vm: (*wsapivm.MyVm) pointer to the VM that we are handeling.
+// err: (error) If we will have some error we can handle it here.
+func CloneVM(vmc *httpclient.HTTPClient, pid string, n string) (*MyVm, error) {
+	var vm MyVm
+	var DataVM CreatePayload
+	DataVM.Name = n
+	DataVM.ParentId = pid
+	requestBody := new(bytes.Buffer)
+	err := json.NewEncoder(requestBody).Encode(DataVM)
+	log.Debug().Msgf("Request Body RAW: %#v", requestBody.String())
+	if err != nil {
+		log.Error().Err(err).Msg("The request JSON is malformed.")
+		return nil, err
+	}
+	response, err := vmc.ApiCall("vms", "POST", *requestBody)
+	if err != nil {
+		log.Error().Err(err).Msg("We can't made the API call.")
+		return nil, err
+	}
+	log.Debug().Msgf("Response RAW: %#v", response)
+	responseBody := new(bytes.Buffer)
+	_, err = responseBody.ReadFrom(response)
+	if err != nil {
+		log.Error().Err(err).Msg("The response JSON is malformed.")
+		return nil, err
+	}
+	log.Debug().Msgf("Response Human Readable: %#v", responseBody.String())
+	err = json.NewDecoder(responseBody).Decode(&vm)
+	if err != nil {
+		log.Error().Err(err).Msg("The response JSON is malformed.")
+		return nil, err
+	}
+	return &vm, nil
+}
+
 // GetVM Auxiliar function to get the data of the VM and don't repeat code
 // Input:
-// c: (*wsapiclient.Client) pointer at the client of the API server.
+// vmc: (*wsapiclient.Client) pointer at the client of the API server.
 // i: (string) string with the ID yo VM
 // Outputs:
 // vm: (*wsapivm.MyVm) pointer to the VM that we are handeling.
@@ -97,7 +138,7 @@ func GetVMbyName(vmc *httpclient.HTTPClient, n string) (*MyVm, error) {
 // functions LoadVM and LoadVMbyName.
 // Inputs:
 // vm: (*wsapivm.MyVm) That's will be the pointer at our vm that we want fill.
-// c: (*wsapiclient.Client) pointer at the client of the API server.
+// vmc: (*httpclient.Client) pointer at the client of the API server.
 // Outputs:
 // err: (error) If we will have some error we can handle it here.
 func GetAllExtraParameters(vmc *httpclient.HTTPClient, vm *MyVm) error {
@@ -116,20 +157,20 @@ func GetAllExtraParameters(vmc *httpclient.HTTPClient, vm *MyVm) error {
 		log.Error().Err(err).Msg("We couldn't show the Power Status.")
 		return err
 	}
-	if vm.PowerStatus == "on" {
-		err = wsapinet.GetInfoNics(vmc, vm.IdVM)
-		if err != nil {
-			log.Error().Err(err).Msg("We couldn't show the Network information.")
-			return err
-		}
-	}
+	// if vm.PowerStatus == "on" {
+	// 	err = wsapinet.GetInfoNics(vmc, vm.IdVM)
+	// 	if err != nil {
+	// 		log.Error().Err(err).Msg("We couldn't show the Network information.")
+	// 		return err
+	// 	}
+	// }
 	return nil
 }
 
 // GetBasicInfo Auxiliary function in charge of getting de Basic Information
 // Inputs:
 // vm: (*wsapivm.MyVm) The VM that we want to know the Memory and CPU info.
-// c: (*wsapiclient.Client) pointer at the client of the API server.
+// vmc: (*httpclient.Client) pointer at the client of the API server.
 // Outputs:
 // err: (error) If we will have some error we can handle it here.
 func GetBasicInfo(vmc *httpclient.HTTPClient, vm *MyVm) error {
@@ -145,6 +186,46 @@ func GetBasicInfo(vmc *httpclient.HTTPClient, vm *MyVm) error {
 	}
 	log.Debug().Msgf("VM: %#v", vm)
 	log.Info().Msg("We have loaded the Processor and Memory values.")
+	return nil
+}
+
+// SetBasicInfo Auxiliary function to add the basic values of a VM Processor and Memory
+// Imputs:
+// vmc: (*httpclient.Client) pointer at the client of the API server.
+// vm: (*wsapivm.MyVm) The VM that we want to Set the Memory and CPU info.
+// p: (string) The CPU settings that we want to put in VM
+// m: (string) The Memory settings that we want in the VM
+// Outputs:
+// err: (error) If we will have some error we can handle it here.
+func SetBasicInfo(vmc *httpclient.HTTPClient, vm *MyVm, p int32, m int32) error {
+	var settings SettingPayload
+	settings.Processors = p
+	settings.Memory = m
+	requestBody := new(bytes.Buffer)
+	err := json.NewEncoder(requestBody).Encode(settings)
+	if err != nil {
+		log.Error().Err(err).Msgf("Trying to encode this request: %#v", settings)
+		return err
+	}
+	log.Debug().Msgf("Request Human Readable: %#v", requestBody.String())
+	response, err := vmc.ApiCall("vms/"+vm.IdVM, "PUT", *requestBody)
+	if err != nil {
+		log.Error().Err(err).Msg("We couldn't complete the API call.")
+		return err
+	}
+	log.Debug().Msgf("Response RAW: %#v", response)
+	responseBody := new(bytes.Buffer)
+	_, err = responseBody.ReadFrom(response)
+	if err != nil {
+		log.Error().Err(err).Msg("We couldn't read the data from the Request.")
+		return err
+	}
+	log.Debug().Msgf("Response Human Readable: %#v", responseBody.String())
+	err = json.NewDecoder(responseBody).Decode(&vm)
+	if err != nil {
+		log.Error().Err(err).Msg("The response JSON is malformed.")
+		return err
+	}
 	return nil
 }
 
@@ -265,7 +346,7 @@ func PowerStateConversor(ops string) (s string) {
 // which is the file of the vm.
 // Inputs:
 // vm: (*wsapivm.MyVm) The VM that we want to know the Denomination and Description info.
-// c: (*wsapiclient.Client) pointer at the client of the API server.
+// c: (*httpclient.HTTPClient) pointer at the client of the API server.
 // p: (string) String with the name or param to set,
 // v: (string) String with the value of param err: variable with error if occur
 // Outputs:
@@ -274,13 +355,12 @@ func SetParameter(vmc *httpclient.HTTPClient, vm *MyVm, p string, v string) erro
 	var param ParamPayload
 	param.Name = p
 	param.Value = v
-	request, err := json.Marshal(param)
+	requestBody := new(bytes.Buffer)
+	err := json.NewEncoder(requestBody).Encode(param)
 	if err != nil {
 		log.Error().Err(err).Msgf("Trying to encode this request: %#v", param)
 		return err
 	}
-	requestBody := new(bytes.Buffer)
-	requestBody.Write(request)
 	log.Debug().Msgf("Request Human Readable: %#v", requestBody.String())
 	response, err := vmc.ApiCall("/vms/"+vm.IdVM+"/configparams", "PUT", *requestBody)
 	if err != nil {
